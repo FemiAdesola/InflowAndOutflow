203188098379209256f919e5ac64aab46a97ccbf
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-11-17 11:58:00 +0200

    creates ReturnType controller

---
+++ CashOutflow/Controllers/AppointmentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CashOutflow.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            //string todaysDate = DateTime.Now.ToShortDateString();
+            //return Ok(todaysDate);
+            return View();
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            return Ok("let see it = {id}" + id);
+        }
+
+    }
+}
+
