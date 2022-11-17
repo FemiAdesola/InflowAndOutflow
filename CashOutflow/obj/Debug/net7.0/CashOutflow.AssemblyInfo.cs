79aa431fba8680aa44aa316b840ca6e809c54803
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-11-17 13:14:31 +0200

    Added docker image and functionality, initialize database

--- CashOutflow/obj/Debug/net7.0/CashOutflow.AssemblyInfo.cs
+++ CashOutflow/obj/Debug/net7.0/CashOutflow.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("78440b27-77f2-4bea-9e7b-410fcf476e68")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashOutflow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
