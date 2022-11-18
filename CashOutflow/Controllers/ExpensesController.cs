f5b97b3635a7568ea78573ce43e1c586e24b5b20
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-11-18 16:24:44 +0200

    Creates delete functionality

--- CashOutflow/Controllers/ExpensesController.cs
+++ CashOutflow/Controllers/ExpensesController.cs
@@ -47,6 +47,42 @@ namespace CashOutflow.Controllers
             }
             return View(obj);
         }
+
+        //Get-Delete ########################################
+        public IActionResult Delete(int? id)
+        {
+            // for passing data to database
+            
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Expenses.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+
+
+        //Post-Delete  ########################################
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            // for passing data to database
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Expenses.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
 }
 
