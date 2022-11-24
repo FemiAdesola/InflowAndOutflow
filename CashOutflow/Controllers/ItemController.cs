7bb9e5608f8bdbb4e485ea85d61b6a0ca34d7a34
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2022-11-24 14:35:28 +0200

    creates update and delete functionality in Item

--- CashOutflow/Controllers/ItemController.cs
+++ CashOutflow/Controllers/ItemController.cs
@@ -38,10 +38,89 @@ namespace CashOutflow.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Item obj)
         {
+            //// for passing data to database
+            //_db.Items.Add(obj);
+            //_db.SaveChanges();
+            //return RedirectToAction("Index");
             // for passing data to database
-            _db.Items.Add(obj);
+
+            if (ModelState.IsValid)
+            {
+                _db.Items.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Get-Delete ########################################
+        public IActionResult Delete(int? id)
+        {
+            // for passing data to database
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Items.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+
+        //Post-Delete  ########################################
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            // for passing data to database
+
+            var obj = _db.Items.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Items.Remove(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
+
+        }
+
+        //Get-update  ########################################
+
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Items.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+
+        //post-update  ########################################
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Item obj)
+        {
+            // for passing data to database
+            if (ModelState.IsValid)
+            {
+                _db.Items.Update(obj);
+                _db.SaveChanges();
+                //return RedirectToAction("Index");
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
         }
     }
 }
